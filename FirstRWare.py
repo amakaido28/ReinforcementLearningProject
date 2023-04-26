c2aa2b0ffa076fce442bcab115480253fd3cf517
amakaido <amakaido@Ubuntu.ubuntu.virtualbox.org>
2023-04-26 21:54:18 +0200

    Working environment with random actions

---
+++ FirstRWare.py
@@ -0,0 +1,29 @@
+import gym
+import rware
+
+layout = """
+.......
+..xx...
+..x.x..
+.x...x.
+..x.x..
+...x...
+.g...g.
+"""
+env=rware.Warehouse(9,1,5,3,2,1,3,5,7,rware.RewardType.GLOBAL)
+episodes=100000
+for episode in range(1,episodes+1): 
+    state=env.reset()
+    done =False
+    score=0
+    while not done:
+        env.render("human")
+        actions = env.action_space.sample()
+        n_state,reward,done,info=env.step(actions)
+        done=False
+        #score+=reward
+    
+env.action_space.sample()
+
+while True:
+    print()
