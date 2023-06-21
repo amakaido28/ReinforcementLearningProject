624a67b5407980a7991a568298732d5192ea0084
amarildo kaido <amarildo.kaido@systemceramics.com>
2023-06-21 22:35:43 +0200

    .

--- A2C/LunarLander_a2c.py
+++ A2C/LunarLander_a2c.py
@@ -12,9 +12,9 @@ class Actor(nn.Module):
         super().__init__()
         self.model = nn.Sequential(
             nn.Linear(input, 64),
-            nn.Tanh(),
+            nn.ReLU(),
             nn.Linear(64, 32),
-            nn.Tanh(),
+            nn.ReLU(),
             nn.Linear(32, output),
             nn.Softmax()
         )
@@ -27,11 +27,10 @@ class Critic(nn.Module):
         super().__init__()
         self.model=nn.Sequential(
             nn.Linear(input,64),
-            nn.Tanh(),
-            nn.Linear(64,32),
-            nn.Tanh(),
-            nn.Linear(32,1),
-            nn.Softmax()
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32,1)
         )
 
     def forward(self,x):
@@ -43,7 +42,7 @@ def main():
 
     gamma=0.99
     ep_rewards = deque(maxlen=100)
-    view_step=30
+    view_step=10
     best_score=-400
     actor_loss=0
     critic_loss=0
@@ -53,7 +52,7 @@ def main():
     state_num = env.observation_space.shape[0]
     action_num = env.action_space.n
 
-    load_weights=True
+    load_weights=False
 
     actor=Actor(state_num,action_num)
     critic=Critic(state_num)
@@ -61,7 +60,7 @@ def main():
     if(load_weights):
         #actor load model
         actor = Actor(state_num,action_num)
-        adam_actor = torch.optim.Adam(actor.parameters(), lr=1e-3)
+        adam_actor = torch.optim.Adam(actor.parameters(), lr=2e-3)
         checkpoint = torch.load('C:\\Users\\kaidoama\\Desktop\\mio\\Reinforcement Learning\\LunarLander\\actor_model.pth')
         actor.load_state_dict(checkpoint['actor_state_dict'])
         adam_actor.load_state_dict(checkpoint['adam_actor_state_dict'])
@@ -71,7 +70,7 @@ def main():
 
         #critic load model
         critic = Critic(state_num)
-        adam_critic = torch.optim.Adam(critic.parameters(), lr=1e-3)
+        adam_critic = torch.optim.Adam(critic.parameters(), lr=2e-3)
         checkpoint = torch.load('C:\\Users\\kaidoama\\Desktop\\mio\\Reinforcement Learning\\LunarLander\\critic_model.pth')
         critic.load_state_dict(checkpoint['critic_state_dict'])
         adam_critic.load_state_dict(checkpoint['adam_critic_state_dict'])
@@ -79,15 +78,16 @@ def main():
         critic_loss = checkpoint['loss_critic']
 
         #make other 1000 steps
-        total_episode = j+4000
+        total_episode = j+10000
     else:
-        total_episode = 100
+        total_episode = 10000
         
-        adam_actor = torch.optim.Adam(actor.parameters(), lr=1e-3)
-        adam_critic = torch.optim.Adam(critic.parameters(), lr=1e-3)
+        adam_actor = torch.optim.Adam(actor.parameters(), lr=2e-3)
+        adam_critic = torch.optim.Adam(critic.parameters(), lr=2e-3)
     
 
-    ep_rewards = deque(maxlen=10)
+    ep_rewards = np.array([])
+    media=0
     for i in range(total_episode):
         done=False
         total_reward=0
@@ -95,65 +95,41 @@ def main():
             env = gym.make("LunarLander-v2",render_mode="human")
         else:
             env = gym.make("LunarLander-v2")
-        state=env.reset()
+        state,_=env.reset()
         
         
         while True:
-            if(len(state)==2):
-                probs=actor(t(state[0]))
-                dist=torch.distributions.Categorical(probs=probs)
-                action=dist.sample()
-                next_state, reward, done, truncated, info = env.step(action.detach().data.numpy())
-                #print(reward)
-                #calculate advantage
-                td_target = reward + gamma * critic(t(next_state)) * (1-done)
-                advantage = td_target - critic(t(state[0]))
-                total_reward += reward
+            probs=actor(t(state))
+            dist=torch.distributions.Categorical(probs=probs)
+            action=dist.sample()
+            next_state, reward, done, truncated, info = env.step(action.detach().data.numpy())
+            #print(reward)
 
-                state = next_state
-
-                #compute loss
-                critic_loss = advantage.pow(2).mean()
-                adam_critic.zero_grad()
-                critic_loss.backward()
-                adam_critic.step()
-
-                actor_loss = -dist.log_prob(action)*advantage.detach()
-                adam_actor.zero_grad()
-                actor_loss.backward()
-                adam_actor.step()
-            else:
-                probs=actor(t(state))
-                dist=torch.distributions.Categorical(probs=probs)
-                action=dist.sample()
-                next_state, reward, done, truncated, info = env.step(action.detach().data.numpy())
-                #print(reward)
-
-                #calculate advantage
-                td_target = reward + gamma * critic(t(next_state)) * (1-done)
-                advantage = td_target - critic(t(state))
+            #calculate advantage
+            td_target = reward + gamma * critic(t(next_state)) * (1-done)
+            advantage = td_target - critic(t(state))
                 
-                total_reward += reward
+            total_reward += reward
 
-                state = next_state
+            state = next_state
 
-                #compute loss
-                critic_loss = advantage.pow(2).mean()
-                adam_critic.zero_grad()
-                critic_loss.backward()
-                adam_critic.step()
+            #compute loss
+            critic_loss = advantage.pow(2).mean()
+            adam_critic.zero_grad()
+            critic_loss.backward()
+            adam_critic.step()
 
-                actor_loss = -dist.log_prob(action)*advantage.detach()
-                adam_actor.zero_grad()
-                actor_loss.backward()
-                adam_actor.step()
+            actor_loss = -dist.log_prob(action)*advantage.detach()
+            adam_actor.zero_grad()
+            actor_loss.backward()
+            adam_actor.step()
     
     
             if done:
-                ep_rewards.append(total_reward)
-                x=round(np.mean(ep_rewards), 3)
+                ep_rewards=np.append(ep_rewards,total_reward)
+                x=round(np.mean(ep_rewards[-60:]), 3)
                 if(x>best_score):
-                    best_score=round(np.mean(ep_rewards), 3)
+                    best_score=round(np.mean(ep_rewards[-60:]), 3)
                     torch.save(
                         {'step': i,
                         'actor_state_dict': actor.state_dict(),
@@ -170,10 +146,13 @@ def main():
 	                    'C:\\Users\\kaidoama\\Desktop\\mio\\Reinforcement Learning\\LunarLander\\critic_model.pth'
                     )
                 
-                
                 if i % 10 == 0:
-                    print("episode: {}\treward: {}".format(i, round(np.mean(ep_rewards), 3)))
+                    print("episode: {}\treward: {}".format(i, total_reward))
                 break
+        media=round(np.mean(ep_rewards[-60:]), 3)
+        if media>=220:
+            print("!!!PROBLEM RESOLVED!!!")
+            break
 
 if __name__ == '__main__':
     main()
